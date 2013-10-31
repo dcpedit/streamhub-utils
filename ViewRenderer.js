94a54c436a07e0f1705ba8ffa9c8f9021d4cb56f
Ming-Gih Lam <minggih@gmail.com>
2013-10-31 11:12:13 -0700

    Add percentage and cssHeight functions

--- ViewRenderer.js
+++ ViewRenderer.js
@@ -2,6 +2,20 @@ define(function(require) {
 var Backbone = require('backbone'),
     Mustache = require('mustache');
 
+function findPercentage() {
+  if (this.width && this.height) {
+    return 100 * (this.height/this.width);
+  }
+}
+
+function calculateHeight() {
+  return function(text, render) {
+    var r = this.height / this.width;
+    var h = parseInt(text || 0);
+    return h ? (Math.ceil(h*r)  + 'px') : 'auto';
+  };
+}
+
 function _normalize(data) {
   if (data.authorId.match(/twitter\.com$/)) {
     data.author.twitterUserId = data.author.id.split(/@/).shift();
@@ -19,6 +33,15 @@ function _normalize(data) {
       delete data.bodyHtml;
     }
   }
+
+  if (data.attachments) {
+    var len = data.attachments.length;
+    while (len--) {
+      data.attachments[len].percentage = findPercentage;
+      data.attachments[len].cssHeight = calculateHeight;
+    }
+  }
+
   return data;
 }
 
